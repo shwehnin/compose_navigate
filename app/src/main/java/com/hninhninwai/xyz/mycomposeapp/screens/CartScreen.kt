0b65977cca67d5b29530c5dcede5b74e87370040
shwehnin <hninhnin6398@gmail.com>
2025-12-24 12:39:04 +0700

    adding the cart screen

---
+++ app/src/main/java/com/hninhninwai/xyz/mycomposeapp/screens/CartScreen.kt
@@ -0,0 +1,24 @@
+package com.hninhninwai.xyz.mycomposeapp.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CartScreen(navigateToProduct: () -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Cart")
+        Button(onClick = navigateToProduct) {
+            Text("Go to Product")
+        }
+    }
+}
